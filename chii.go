ce9b3d70e8e972e2059f045da02161c66eed52f7
anon <na>
2022-12-09 09:52:45 -0500

    yt link pre text accounted for

--- chii.go
+++ chii.go
@@ -134,7 +134,7 @@ func Openweather(stored string, conn *irc.Connection) {
 
 }
 
-var youtube_linkreg = regexp.MustCompile(`(https:\/\/|https:\/\/www.)(youtube.com\/watch\?v=|youtu.be\/)(\S+)`)
+var youtube_linkreg = regexp.MustCompile(`(?:.*)(https:\/\/|https:\/\/www.)(youtube.com\/watch\?v=|youtu.be\/)(\S+)`)
 
 //posts title and description of youtube videos
 func Youtube(stored string, conn *irc.Connection) {
